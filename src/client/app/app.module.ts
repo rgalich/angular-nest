37606d7fef70f522485b53f031f3fa48e54ebc9f
Rémi GALICHON <Remi.GALICHON@Equadex.net>
2018-10-11 13:33:21 +0200

    add ng-zorro-antd

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgZorroAntdModule, NZ_I18N, fr_FR } from 'ng-zorro-antd';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +9,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgZorroAntdModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: fr_FR }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
