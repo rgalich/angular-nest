2fbb3ad8d24255705898d96e37f595461bf6f340
remi <remigalichon@gmail.com>
2018-10-11 18:59:12 +0200

    init routes

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgZorroAntdModule, NZ_I18N, fr_FR } from 'ng-zorro-antd';
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    NgZorroAntdModule
+    NgZorroAntdModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [{ provide: NZ_I18N, useValue: fr_FR }],
   bootstrap: [AppComponent]
