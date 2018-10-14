51343b92e2dbc018aac9225e1770499e2b825e1d
remi <remigalichon@gmail.com>
2018-10-14 10:24:05 +0200

    user

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot(),
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
