8d4c921284de85116b337c126ec52bb45b14c0e9
Rémi GALICHON <Remi.GALICHON@Equadex.net>
2018-10-22 14:09:41 +0200

    wip test login

--- src/client/app/authentication/login/login.component.spec.ts
+++ src/client/app/authentication/login/login.component.spec.ts
@@ -1,35 +1,51 @@
 import { LoginComponent } from './login.component';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NgZorroAntdModule } from 'ng-zorro-antd';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { AuthService } from 'app/service/auth.service';
-import { CommonModule } from '@angular/common';
 import { AuthenticationModule } from '../authentication.module';
+import { FormControlsService } from 'app/core/service/form-controls.service';
 
-let component: LoginComponent;
-let fixture: ComponentFixture<LoginComponent>;
-let button: HTMLElement;
+describe('LoginComponent', () => {
 
-beforeEach(() => {
     const fakeRouter = jasmine.createSpyObj('Router', ['navigate']);
     const fakeAuthService = jasmine.createSpyObj('AuthService', ['login']);
-    TestBed.configureTestingModule({
-        imports: [
-            AuthenticationModule
-        ],
-        providers: [
-            { provide: AuthService, useValue: fakeAuthService },
-            { provide: Router, useValue: fakeRouter }
-          ]
+    const fakeFormControlsService = jasmine.createSpyObj('FormControlsService', ['validateControls']);
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                AuthenticationModule
+            ],
+            providers: [
+                { provide: AuthService, useValue: fakeAuthService },
+                { provide: Router, useValue: fakeRouter },
+                { provide: FormControlsService, useValue: fakeFormControlsService },
+              ]
+        });
     });
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
+    
+    it('should display original title', () => {
+        const fixture = TestBed.createComponent(LoginComponent);
+        fixture.detectChanges();
+        const componentInstance = fixture.componentInstance;
+        
+        const element = fixture.nativeElement;
 
-    button = fixture.nativeElement.querySelector('button');
-});
+        const mailInput = element.querySelector('[formcontrolname="mail"]');
+        // mailInput.value = 'mail';
+        // mailInput.dispatchEvent(new Event('input'));
+        const passwordInput = element.querySelector('[formcontrolname="password"]');
+        // passwordInput.value = 'password';
+        // passwordInput.dispatchEvent(new Event('input'));
 
-it('should display original title', () => {
-    fixture.detectChanges();
-    expect(button.textContent).toContain('Connexion');
-});
+        componentInstance.submitForm();
+        fixture.detectChanges();
+
+        expect(mailInput).not.toBeNull('You should have an input mail');
+        expect(mailInput.classList.contains("ng-invalid")).toBe(true, 'The button should be enabled if the form is valid');
+        expect(passwordInput).not.toBeNull('You should have an input password');
+        expect(passwordInput.classList.contains("ng-invalid")).toBe(true, 'The button should be enabled if the form is valid');
+        
+        expect(componentInstance.validateForm.valid).toBe(false, 'The button should be enabled if the form is valid');
+    });
+})
