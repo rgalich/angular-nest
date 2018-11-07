5770a76502b2881e92d85e0e7afe6b32405e08d1
Rémi GALICHON <Remi.GALICHON@Equadex.net>
2018-11-07 09:22:51 +0100

    add user component

---
+++ src/client/app/user/user.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListComponent } from './list/list.component';
+import { UserComponent } from './user/user.component';
+
+@NgModule({
+  declarations: [ListComponent, UserComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserModule { }
